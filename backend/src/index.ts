2fe6931ab0180dea9d4af977d49da3e3c7dc300c
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2025-12-12 13:05:24 +0530

    logical things done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,23 @@
+import 'dotenv/config'
 import express, { type Request, type Response } from "express"
 import OpenAI from "openai"
+import { clerkClient, clerkMiddleware, getAuth, requireAuth } from "@clerk/express"
+import cors from "cors"
 const app = express()
 const client = new OpenAI({
     apiKey : process.env.OPENAI_API_KEY
 })
 
+app.use(cors())
+
 
 
 app.use(express.json());
 
-app.get("/", async(req : Request , res : Response)=> {
+app.get("/",  async(req : Request , res : Response)=> {
+
+
+
 
      res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
@@ -33,13 +41,13 @@ app.get("/", async(req : Request , res : Response)=> {
         continue
        }
 
-       // split into lines (like your Python code)
+
     const lines = text.split("\n");
 
     for (const line of lines) {
       res.write(`data: ${line}\n`);
     }
-    res.write("\n"); // blank line ends SSE event
+    res.write("\n"); 
   }
       // when finished
   res.write("event: end\ndata: done\n\n");
